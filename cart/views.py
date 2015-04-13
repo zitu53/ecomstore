3633a78a7701b18293183b5def514090371d5bd0
zitu46 <arif.arman046@gmail.com>
2015-04-13 17:42:53 +0600

    cart modified, update remove added : pc

--- cart/views.py
+++ cart/views.py
@@ -3,5 +3,12 @@ from django.shortcuts import render
 from cart import cart
 # Create your views here.
 def show_cart(request):
-    cart_item_count = cart.cart_distinct_item_count(request)
+    if request.method == 'POST':
+        postdata = request.POST.copy()
+        if postdata['submit'] == 'Remove':
+            cart.remove_from_cart(request)
+        if postdata['submit'] == 'Update':
+            cart.update_cart(request)
+    cart_items = cart.get_cart_items(request)
+    cart_subtotal = cart.cart_subtotal(request)
     return render(request, "cart/cart.html", locals())
